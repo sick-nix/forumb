b8949f679322c3a553bd2083b4d914b36eecdd6a
sick-nix <nicolae.gr451@gmail.com>
2024-10-25 12:32:46 +0200

    typo

--- config/auth.php
+++ config/auth.php
@@ -112,7 +112,7 @@ return [
 
     'password_timeout' => env('AUTH_PASSWORD_TIMEOUT', 10800),
 
-    'default_admin_username' => env('ADMIN_USERNAME', 'admin'),
-    'default_admin_email' => env('ADMIN_EMAIL', 'admin@fakemail.com'),
+    'admin_username' => env('ADMIN_USERNAME', 'admin'),
+    'admin_email' => env('ADMIN_EMAIL', 'admin@fakemail.com'),
     'admin_password' => env('ADMIN_PASSWORD')
 ];
